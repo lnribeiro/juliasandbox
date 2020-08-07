6ee631f3cb3483fa31cb9418a152bcc14d7305ca
Lucas Nogueira Ribeiro <lucasnogrib@gmail.com>
2020-08-07 12:11:05 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # juliasandbox
+
+Julia sandbox to learn how to use it. Does it make sense to use Julia instead of MATLAB or Python? Let's find out!
---
+++ scripts/variables.jl
@@ -0,0 +1,30 @@
+# This is a comment
+
+x = 1
+typeof(x)
+
+name = "Lucas"
+typeof(name)
+
+δ = 0.00001     # UTF8 symbols are supported!
+println(δ)
+
+# stylistic conventions:
+# variables are lowercase
+# Types and Modules begin with a capital letter (camel style)
+
+y = 1e-4
+typeof(y)
+
+bitstream = 0b10010
+typeof(bitstream)
+
+# we dont need to print to show some calculation...
+1 + 2
+
+# division by zero does not raise an error
+1/0
+
+q = 3
+y = q^2 + -6*q + 9
+print(y)
